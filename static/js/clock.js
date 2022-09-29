a2553d71e13ee3d16413cda2311fbf6204a51dd8
Elena Hoshizaki <elena.hoshizaki@gmail.com>
2022-09-29 00:01:10 -0700

    I4: Add time input for alarm

---
+++ static/js/clock.js
@@ -0,0 +1,22 @@
+console.log('HELLO');
+// const pageBody = document.getElementById("guiBody");
+//
+// console.log("hello ", pageBody);
+// // pageBody.onload = function() { startTime() };
+//
+// // clock
+// function startTime() {
+//     const today = new Date();
+//     let h = today.getHours();
+//     let m = today.getMinutes();
+//     let s = today.getSeconds();
+//     m = checkTime(m);
+//     s = checkTime(s);
+//     document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+//     setTimeout(startTime, 1000);
+// }
+//
+// function checkTime(i) {
+//     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+//     return i;
+// }
