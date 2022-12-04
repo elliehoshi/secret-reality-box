ea832355b0f87d7b89a7495baeed146fefdf0fa6
Elena Hoshizaki <elena.hoshizaki@gmail.com>
2022-12-03 16:47:43 -0800

    merged ocr_thonny file with main.py

--- main.py
+++ main.py
@@ -1,203 +1,112 @@
-from asyncio.exceptions import CancelledError
-from asyncio.tasks import ALL_COMPLETED
-import logging
-import asyncio
-import os
-from typing import List
+import picamera     #camera library
+import pygame as pg #audio library
+import os           #communicate with os/command line
+import numpy as np
 
-# Crickit - Servo and NeoPixel
+from google.cloud import vision  #gcp vision library
+from time import sleep
 from adafruit_crickit import crickit
-# Drive NeoPixels on the NeoPixels Block on Crickit FeatherWing
-from adafruit_seesaw.neopixel import NeoPixel
+import time
+import signal
+import sys
+import re           #regular expression lib for string searches!
+import subprocess
 
-import picamera
-from starlette.websockets import WebSocket
-from uvicorn import Config, Server
-from starlette.applications import Starlette
-from starlette.endpoints import WebSocketEndpoint
-from starlette.middleware import Middleware
-from starlette.middleware.gzip import GZipMiddleware
-from starlette.routing import Mount, WebSocketRoute
-from starlette.staticfiles import StaticFiles
-from uvicorn.config import LOGGING_CONFIG
-from google.cloud import vision
-import pygame 
-from gtts import gTTS
-import re   #regex lib for string searches
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="vision-voice-key.json"
+#set up your GCP credentials - replace the " " in the following line with your .json file and path
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="Zack-Cloud.json"
 
-LOG_FORMAT = "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
-logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
-LOGGING_CONFIG["formatters"]["default"]["fmt"] = LOG_FORMAT
-LOGGING_CONFIG["formatters"]["access"][
-    "fmt"
-] = "%(asctime)s | %(levelname)s | %(name)s | %(client_addr)s | %(request_line)s | %(status_code)s"
-LOGGING_CONFIG["loggers"]["uvicorn.error"]["handlers"] = ["default"]
-LOGGING_CONFIG["loggers"]["uvicorn.error"]["propagate"] = False
-logger = logging.getLogger("main")
+# this line connects to Google Cloud Vision! 
+client = vision.ImageAnnotatorClient()
 
-websockets: List[WebSocket] = []
-camera = picamera.PiCamera()
-vision_client = vision.ImageAnnotatorClient()
+# global variable for our image file - to be captured soon!
+image = 'image.jpg'
 
-# cuckoo sound file
-cuckoo_sound = "cuckoo-clock-sound.mp3"
+def takephoto(camera):
+    
+    # this triggers an on-screen preview, so you know what you're photographing!
+    camera.start_preview() 
+    sleep(.5)                   #give it a pause so you can adjust if needed
+    camera.capture('image.jpg') #save the image
+    camera.stop_preview()       #stop the preview
 
-async def broadcast(*args, **kwargs):
-    value = args[0]
-    if len(websockets) > 0:
-        logger.info("Sending: " + value)
-        await asyncio.wait(
-            [asyncio.create_task(ws.send_text(value)) for ws in websockets],
-            timeout=3,
-            return_when=ALL_COMPLETED,
-        )
+def ocr_handwriting(image):
+    #this function sends your image to google cloud using the
+    #text_detection method, collects a response, and parses that
+    #response for all of the associated words detected.
+    #these are captured as a single joined string in word_text.
+    #if there is handwriting detected, strings are sent to motor_turn()
+    #to determine if and how the motor should actuate!
+    
+    #these two lines connect to google cloud vision in the text_detection mode
+    response = client.text_detection(image=image)
+    text = response.full_text_annotation
+    
+    word_text = ""
+    
+    #this next block of code parses google cloud's response
+    #down to words detected, which are combined into word_text
+    for page in text.pages:
+        for block in page.blocks:
+            for paragraph in block.paragraphs:
+                for word in paragraph.words:
+                    word_text += " "
+                    word_text += ''.join([
+                        symbol.text for symbol in word.symbols
+                        ])                    
 
-async def poll_camera():
-    while True:
-#         camera.start_preview()
-        await asyncio.sleep(.5)
-        camera.capture('image.jpg')
-#         camera.stop_preview()
+    #this next block checks if any word text was detected - and
+    #if it was, the text and search strings are sent to motor_turn()
 
-        with open('image.jpg', 'rb') as image_file:
-            content = image_file.read()
-            response = vision_client.label_detection(image=vision.Image(content=content))
-            labels = response.label_annotations
+    if word_text:
+        return word_text
+    else:
+        return 0
 
-            label_text = ""
-
-            # parse labels, extract desc and stringify
-            for label in labels:
-                label_text += ''.join([label.description, " "])
-
-            # send sound files, search strings, and label text to speaker_out()
-            if label_text:
-               print('image_labeling(): {}'.format(label_text))
-            else:
-                print('image_labeling(): No Label Descriptions')
-
-#             for label in labels:
-#                 await broadcast("Tags: {}".format(label.description))
-
-        await asyncio.sleep(1)
-
-# servo code
-async def motor_wait():
-     while True:
-        print("Moving servo #1: motor_wait()")
-        crickit.servo_1.angle = 0      # right
-        await asyncio.sleep(1)
-        crickit.servo_1.angle = 90     # middle
-        await asyncio.sleep(1)
-        crickit.servo_1.angle = 180    # left
-        await asyncio.sleep(1)
-        crickit.servo_1.angle = 90     # middle
-        await asyncio.sleep(1)
-        crickit.servo_1.angle = 0     # right
-# end servo code
-
-# nanoring NeoPixel code
-num_pixels = 30  # Number of pixels driven from Crickit NeoPixel terminal
-
-# The following line sets up a NeoPixel strip on Seesaw pin 20 for Feather
-pixels = NeoPixel(crickit.seesaw, 20, num_pixels)
-
-def wheel(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colors are a transition r - g - b - back to r.
-    if pos < 0 or pos > 255:
-        return (0, 0, 0)
-    if pos < 85:
-        return (255 - pos * 3, pos * 3, 0)
-    if pos < 170:
-        pos -= 85
-        return (0, 255 - pos * 3, pos * 3)
-    pos -= 170
-    return (pos * 3, 0, 255 - pos * 3)
-
-async def neopixel_rainbow():
-    while True:
-        print("neopixel rainbow")
-        for j in range(255):
-                for i in range(num_pixels):
-                    rc_index = (i * 256 // num_pixels) + j
-                    pixels[i] = wheel(rc_index & 255)
-                pixels.show()
-                await asyncio.sleep(1)
-# end nanoring NeoPixel code
-
-class DataEndpoint(WebSocketEndpoint):
-    async def on_connect(self, websocket: WebSocket) -> None:
-        await websocket.accept()
-        websockets.append(websocket)
-        logger.info("Connected to %s", websocket)
-
-    async def on_disconnect(self, websocket: WebSocket, close_code: int) -> None:
-        websockets.remove(websocket)
-        logger.warning(
-            "Disconnected from websocket %s with code %s", websocket, close_code
-        )
-        await websocket.close()
-
-    async def on_receive(self, websocket: WebSocket, data: str) -> None:
-        logger.info("Socket: %s, Message: %s", websocket, data)
-        if data is not None:
-            pygame.mixer.init()
-            if data == "cuckoo":
-                # move cuckoo bird
-                print("Moving servo #1: motor_wait()")
-                crickit.servo_1.angle = 0      # right
-                await asyncio.sleep(.1)
-                crickit.servo_1.angle = 90     # middle
-                await asyncio.sleep(.1)
-                crickit.servo_1.angle = 180    # left
-                await asyncio.sleep(.1)
-                crickit.servo_1.angle = 90     # middle
-                await asyncio.sleep(.1)
-                crickit.servo_1.angle = 0      # right
-
-                # play cuckoo sound
-                pygame.mixer.music.load(cuckoo_sound) # load sound file
-                pygame.mixer.music.play()       # play sound file
-            else:
-                t2s = gTTS(data, lang ='en')
-                t2s.save('speech.mp3')
-                pygame.mixer.music.load('speech.mp3')
-                pygame.mixer.music.play()
-                while pygame.mixer.music.get_busy():
-                    pygame.time.Clock().tick(10)
-
-app = Starlette(
-    routes=[
-        WebSocketRoute("/ws", endpoint=DataEndpoint),
-        Mount("/", app=StaticFiles(directory="static", html=True), name="static"),
-    ],
-    middleware=[Middleware(GZipMiddleware, minimum_size=1000)],
-)
-
-server = Server(
-    config=Config(
-        host="localhost",
-        port=21489,
-        app=app,
-    )
-)
-
-async def event_loop():
-    await asyncio.gather(
-        asyncio.create_task(server.serve()),
-        asyncio.create_task(poll_camera()),
-        asyncio.create_task(neopixel_rainbow())
-    )
 
 def main():
-    try:
-        asyncio.run(event_loop())
-    except CancelledError as er:
-        pass
+    prev_outputs = []
+    counter = 0
+    #generate a camera object for the takephoto function to
+    #work with
+    camera = picamera.PiCamera()
+    sleep_time = 0.1
+    #setup our pygame mixer to play audio in subsequent stages
+  #  pg.init()
+  #  pg.mixer.init()
+    
+    #this while loop lets the script run until you ctrl+c (command line)
+    #or press 'stop' (Thonny IDE)
+    while True:
+ 
+        takephoto(camera) # First take a picture
+        """Run a label request on a single image"""
 
-# the below 'if' statement helps python distinguish the main function.
-if __name__ == "__main__":
-    main()
+        with open('image.jpg', 'rb') as image_file:
+            #read the image file
+            content = image_file.read()
+            #convert the image file to a GCP Vision-friendly type
+            image = vision.Image(content=content)
+            output = ocr_handwriting(image)
+            
+            if output != 0:
+                prev_outputs.append(output)
+                counter += 1
+                print(output)
+                sleep_time = 0.01
+            
+            elif output == 0: # No text detected anymore
+                counter = 0
+                prev_outputs = []
+            
+                
+            if counter == 10: #text is detected
+                values, counts = np.unique(prev_outputs, return_counts=True)
+                ind = np.argmax(counts)
+                most_likely_candidate = values[ind]
+                print('Most likely handwriting(): {}'.format(most_likely_candidate))
+                
+            time.sleep(sleep_time)        
+        
+if __name__ == '__main__':
+        main()
