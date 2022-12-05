39fce9d0d3e871141a92833781881cce86065caf
Elena Hoshizaki <elena.hoshizaki@gmail.com>
2022-12-05 00:58:06 -0800

    iterate through all UI states when scan button is pressed

--- static/js/main.js
+++ static/js/main.js
@@ -4,23 +4,37 @@ function toggleView(showStatus) {
   const scanSuccessWrapper = document.getElementById("scan-successful-wrapper");
   const defaultWrapper = document.getElementById("default-wrapper");
   const scanningWrapper = document.getElementById("scanning-wrapper");
+  const communityWrapper = document.getElementById("community-wrapper");
   const checkmark = document.getElementsByClassName("checkmark")[0];
 
   if ( !showStatus ) {
+    // Default screen
     scanSuccessWrapper.classList.remove("show");
     defaultWrapper.classList.add("show");
     checkmark.classList.add("hide");
     scanningWrapper.classList.remove("show");
-  } else if ( showStatus == "success" ) {
-    defaultWrapper.classList.remove("show");
-    scanSuccessWrapper.classList.add("show");
-    scanningWrapper.classList.remove("show");
-    checkmark.classList.remove("hide");
-    checkmark.classList.add("show");
   } else if ( showStatus == "scanning" ) {
+    // Show "scanning..." animation
     defaultWrapper.classList.remove("show");
-    scanSuccessWrapper.classList.remove("show");
     scanningWrapper.classList.add("show");
+    // After 5 seconds, show Success screen
+    setTimeout(() => {
+      scanSuccessWrapper.classList.add("show");
+      scanningWrapper.classList.remove("show");
+      checkmark.classList.remove("hide");
+      checkmark.classList.add("show");
+    }, 5000)
+    // Then, after 4 more seconds, show "Join the Community" screen
+    setTimeout(() => {
+      scanSuccessWrapper.classList.remove("show");
+      checkmark.classList.add("hide");
+      communityWrapper.classList.add("show");
+    }, 9000)
+    // Wait a few more seconds, then return to the Default screen
+    setTimeout(() => {
+      communityWrapper.classList.remove("show");
+      defaultWrapper.classList.add("show");
+    }, 25000)
   } else {
     console.log('toggleView ', showStatus);
   }
